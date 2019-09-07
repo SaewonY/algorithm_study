e27f00fba5620e250c8170fd8493052313e92b0b
YangSaewon <saewon@DESKTOP-1971B7C.localdomain>
2019-09-07 17:38:36 +0900

    add 6 problems

---
+++ problems/problem_6.py
@@ -0,0 +1,27 @@
+'''
+문제: 숫자 뒤집기
+
+조건1: 정수의 자릿수를 계산 하는 방식을 고민해야 한다.
+조건2: 이 문제의 경우는 반복문보다는 재귀 호출을 응용하는 것이 간단하다.
+
+힌트: 정수의 자릿수를 계산하는 방법을 고민해야 한다.
+'''
+
+
+# def flip_int(n):
+#     n = int(str(n)[::-1])
+#     return n
+
+def solve(n):
+    if n == 0:
+        return 0
+    print(n%10, end = ' ')
+    solve(n//10)
+
+
+if __name__ == '__main__':
+
+    num = input("뒤집을 숫자를 입력하시오: ")
+    # num = flip_int(num)
+    # print(num)    
+    solve(int(num))
