c2f341263a2b04f276ff758a1cb7498797ce44d8
Chris <chrisyang9305@gmail.com>
2019-09-27 23:17:25 +0900

    add 10989

---
+++ baekjoon/sort/problem_10989.py
@@ -0,0 +1,23 @@
+'''
+문제
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+'''
+
+import sys
+
+N = int(input())
+result = [0 for i in range(10001)]
+
+for num in sys.stdin:
+    result[int(num)] += 1
+
+for i in range(10001):
+    if result[i] > 0:
+        for j in range(result[i]):
+            print(i)
