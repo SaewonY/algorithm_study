2bfb8c0ff43dd5b931c22dba37c04e46ee4ad7a7
SaewonY <chrisyang9305@gmail.com>
2019-12-27 14:22:25 +0900

    add sum_min_max.sql

---
+++ programmers_sql/sum_min_max.sql
@@ -0,0 +1,23 @@
+-- Challenge 1: 최댓값 구하기
+SELECT
+    DATETIME AS '시간'
+FROM ANIMAL_INS
+ORDER BY DATETIME DESC
+LIMIT 1;
+
+-- Challenge 2: 최솟값 구하기 
+SELECT
+    DATETIME AS '시간'
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
+
+-- Challenge 3: 동물 수 구하기 
+SELECT
+    COUNT(*)
+FROM ANIMAL_INS;
+
+-- Challenge 3: 중복 제거하기 
+SELECT
+    COUNT(DISTINCT(Name)) AS count
+FROM ANIMAL_INS;
