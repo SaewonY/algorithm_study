d8b41a6055c0755812183481d3753641befa5664
SaewonY <chrisyang9305@gmail.com>
2019-12-31 16:33:55 +0900

    fix pys

--- programmers/problem_09.py
+++ programmers/problem_09.py
@@ -2,11 +2,12 @@
 가장 큰 정사각형 찾기
 
 문제 설명
-1와 0로 채워진 표(board)가 있습니다. 표 1칸은 1 x 1 의 정사각형으로 이루어져 있습니다. 표에서 1로 이루어진 가장 큰 정사각형을 찾아 넓이를 return 하는 solution 함수를 완성해 주세요. (단, 정사각형이란 축에 평행한 정사각형을 말합니다.)
+1와 0로 채워진 표(board)가 있습니다. 표 1칸은 1 x 1 의 정사각형으로 이루어져 있습니다. 
+표에서 1로 이루어진 가장 큰 정사각형을 찾아 넓이를 return 하는 solution 함수를 완성해 주세요. 
+(단, 정사각형이란 축에 평행한 정사각형을 말합니다.)
 
 예를 들어
-
-1	2	3	4
+1	0	1	1
 0	1	1	1
 1	1	1	1
 1	1	1	1
@@ -42,4 +43,7 @@ def solution(board):
                 board[x][y] = min(board[x-1][y-1], min(board[x-1][y], board[x][y-1])) + 1
     return max([item for row in board for item in row])**2
 
-    
+input = [[1,1,0,1],[0,1,1,1], [1,1,1,1], [1,1,1,1],[0,0,1,0]]
+
+result = solution(input)
+print(result)
