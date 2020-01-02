e36ccd3a70387b9f59262a6841f319852f4cd7f9
SaewonY <chrisyang9305@gmail.com>
2020-01-03 04:06:40 +0900

    fix

--- programmers_sql/sum_min_max.sql
+++ programmers_sql/sum_min_max.sql
@@ -5,6 +5,7 @@ FROM ANIMAL_INS
 ORDER BY DATETIME DESC
 LIMIT 1;
 
+
 -- Challenge 2: 최솟값 구하기 
 SELECT
     DATETIME AS '시간'
@@ -12,12 +13,17 @@ FROM ANIMAL_INS
 ORDER BY DATETIME
 LIMIT 1;
 
+
 -- Challenge 3: 동물 수 구하기 
 SELECT
     COUNT(*)
 FROM ANIMAL_INS;
 
--- Challenge 3: 중복 제거하기 
+
+-- Challenge 4: 중복 제거하기 
 SELECT
     COUNT(DISTINCT(Name)) AS count
 FROM ANIMAL_INS;
+
+
+-- Challenge 5: 중복 제거하기 
