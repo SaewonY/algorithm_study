ce24ad1b625535b1c925f8c6f379195405ffe101
SaewonY <chrisyang9305@gmail.com>
2020-01-06 19:52:15 +0900

    add 4 5

---
+++ leetcode/problem_005.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        def helper(i, j):
+            while i >=0 and j < len(s) and s[i]==s[j]:
+                i -= 1
+                j += 1
+            return s[i+1:j]  # s[i] != s[j]
+
+        return s and max((a for i in range(len(s)) 
+                          for a in (helper(i, i), helper(i, i+1))), key=len) or ''
+            
+
+input = "babad"
+A = Solution()
+print(A.longestPalindrome(input))
