0b7821b11cd7c20beac8422d63e8391f73328e33
SaewonY <chrisyang9305@gmail.com>
2020-01-14 15:30:10 +0900

    add problem

---
+++ leetcode/problem_018.py
@@ -0,0 +1,45 @@
+'''
+4Sum
+https://leetcode.com/problems/4sum/
+'''
+
+
+def solution(nums, target):
+
+
+    nums.sort()
+    print(nums)
+    length = len(nums)
+
+    collected = []
+
+    for i in range(length-3):
+        for j in range(i+1, length-2):
+            
+
+            first = nums[i]
+            second = nums[j]
+            left_idx, right_idx = j+1, length-1
+            
+            while left_idx < right_idx:
+                total = first + second + nums[left_idx] + nums[right_idx]
+
+                if total == target:
+                    solution = [first, second, nums[left_idx], nums[right_idx]]
+                    if solution not in collected:
+                        collected.append(solution)
+                
+                if total < target:
+                    left_idx += 1
+                else:
+                    right_idx -= 1
+    
+    collected = set(collected)
+
+    return collected
+
+
+# input = [1, 0, -1, 0, -2, 2]
+input = [-3, -2, -1, 0, 0, 1, 2, 3]
+target = 0
+print(solution(input, target))
